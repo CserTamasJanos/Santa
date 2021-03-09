6263921375739001506bc27f1e239d2989f2d087
CserTamasJanos <70898590+CserTamasJanos@users.noreply.github.com>
2021-03-09 15:56:25 +0100

    javasScript1
    I have to find out how functions and loops are working in js.

--- mikulas.js
+++ mikulas.js
@@ -1,12 +1,31 @@
-function RadioEnable(radioButtonList, radioButton)
+var radio1 = [];
+var radio2 = [];
+var radio3 = [];
+var radio4 = [];
+var radio5 = [];
+
+
+function RadioSwitchTrue(radioButtonsList, radioButton)
 {
-    for(let rButton in radioButtonList)
+    for(let rButton in radioButtonsList)
     {
        
     }
-
-
-
 }
+
+function GetRadioButtons(radioButtonsList)
+{
+    for(var i = 0; i < this.elements.length; i++)
+    {
+        var actualElement = this.elements[i].id .document.getElementById;
+        if(actualElement.document.)
+        {
+
+        }
+
+    }
+}
+
+
 
 
